6846af274a87763e14d10fd6bfc6fec2a7aa5a07
ALVARO FIDALGO <alvarofidalgo@ALVAROs-MacBook-Pro.local>
2017-11-05 20:47:41 +0100

    remove actualLetter from StringWithConsecutive

--- src/main/java/examples/counts/strings/ConsecutiveLetterCounter.java
+++ src/main/java/examples/counts/strings/ConsecutiveLetterCounter.java
@@ -9,24 +9,23 @@ public class ConsecutiveLetterCounter {
     public String convert(String entry) {
 
         StringWithConsecutive a =Arrays.stream(entry.split(""))
-                .map((b)-> new StringWithConsecutive(new ResultString(b,new Head(b, Optional.of(1))),b))
-                .reduce(new StringWithConsecutive(new ResultString("",new Head("",Optional.empty())),""),
+                .map((b)-> new StringWithConsecutive(new ResultString(b,new Head(b, Optional.of(1)))))
+                .reduce(new StringWithConsecutive(new ResultString("",new Head("",Optional.empty()))),
                         (result,head) -> {
 
                     String tail = "";
-                    Integer size = 0;
                     Head headS = null;
-                    if (head.actualLetter.equals(result.actualLetter)) {
+                    if (head.result.head.actualLetter.equals(result.result.head.actualLetter)) {
                         tail = result.result.tail;
-                        size = head.result.head.ocurrece() + result.result.head.ocurrece();
-                        headS = new Head(head.actualLetter,Optional.of(size));
+                        Integer size = head.result.head.ocurrece() + result.result.head.ocurrece();
+                        headS = new Head(head.result.head.actualLetter,Optional.of(size));
                     }else {
                         tail = result.result.tail.concat(result.result.head.toString());
-                        size = head.result.head.ocurrece();
-                        headS = new Head(head.actualLetter,Optional.of(size));
+                        Integer size = head.result.head.ocurrece();
+                        headS = new Head(head.result.head.actualLetter,Optional.of(size));
                     }
                     ResultString res =  new ResultString(tail, headS);
-                    return new StringWithConsecutive(res, head.actualLetter);
+                    return new StringWithConsecutive(res);
 
                         });
             return a.result.toString();
--- src/main/java/examples/counts/strings/StringWithConsecutive.java
+++ src/main/java/examples/counts/strings/StringWithConsecutive.java
@@ -2,11 +2,9 @@ package examples.counts.strings;
 
 public class StringWithConsecutive {
 
-    public final String actualLetter;
     public final ResultString result;
 
-    public StringWithConsecutive(ResultString result,String actualLetter) {
+    public StringWithConsecutive(ResultString result) {
         this.result = result;
-        this.actualLetter = actualLetter;
     }
 }
